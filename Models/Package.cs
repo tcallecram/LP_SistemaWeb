c606188dd5a9d6741a4ce2137ea7d9948dcfd2d1
tcallecram <118746940+tcallecram@users.noreply.github.com>
2024-12-02 18:26:19 -0300

    PDMI6- Trabalho Prático 03

---
+++ Models/Package.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp2.Models
+{
+    public class Package
+    {
+        public string PackageId { get; set; }
+        public string Status { get; set; }
+        public DateTime ShippingDate { get; set; }
+        public DateTime EstimatedDeliveryDate { get; set; }
+        public string CurrentLocation { get; set; }
+        public List<string> EventHistory { get; set; }
+    }
+}
---
+++ Views/TrackingPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MauiApp2.ViewModels;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace MauiApp2.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TrackingPage : ContentPage
+    {
+        public TrackingPage()
+        {
+            InitializeComponent();
+            BindingContext = new PackageViewModel();
+        }
+    }
+}
